69758e501c73667669738863dc3093471c6f7fe8
tetigi <luke.tomlin@gmail.com>
2017-12-25 14:09:38 +0000

    Initial commit

---
+++ project/src/main/kotlin/com/tetigi/project/Main.kt
@@ -0,0 +1,12 @@
+package com.tetigi.project
+
+class Main {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            println("hello, world!")
+        }
+    }
+}
